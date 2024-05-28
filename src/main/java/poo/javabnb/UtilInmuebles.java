c429703e3f31d4c39c1b4f3395d543d84f89c670
eva <eva@localhost>
2024-05-28 11:06:16 +0200

    javadoc operativo, a falta de que salga el html

--- src/main/java/poo/javabnb/UtilInmuebles.java
+++ src/main/java/poo/javabnb/UtilInmuebles.java
@@ -24,6 +24,7 @@ import java.util.Comparator;
  * 
  * @author Alejandro
  */
+
 public class UtilInmuebles implements Serializable  {
      
     private static  ArrayList<Inmuebles> inmuebles = new ArrayList<>();
